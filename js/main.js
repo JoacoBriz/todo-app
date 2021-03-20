cac756cf468bd3841ad06c6d46604347ab3c2a62
Joaquin Brizuela <joaquinbriz1@gmail.com>
2021-03-20 19:02:02 -0300

    addTask and Button Remove;

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
-let buttonAddTask = document.getElementById("buttonTask");
-buttonAddTask.addEventListener("click", takeData);
+document.addEventListener("DOMContentLoaded", function() {
+  const buttonAddTask = document.querySelector("#buttonTask");
+  buttonAddTask.addEventListener("click", takeData);
+  let taskListPending = [];
+  let id = 0;
 
-var taskList = [];
-console.log(taskList);
+  function takeData () {
+    let titleTask = document.querySelector("#titleTask").value;
 
-function takeData () {
-  let titleTask = document.getElementById("titleTask").value;
+    let templateTask = `
+    <li id="taskLi" class="task">
+      <p class="title">${titleTask}</p>
+    </li>
+    `;
 
-  let templateTask = `
-  <li>
-  <h3>${titleTask}</h3>
-  </li>
-  `;
+    if (titleTask == "") {
+      alert("Complete field");
+    } else {
+      //New Task
+      let newTask = document.querySelector("#tasks");
+      newTask.innerHTML += templateTask;
 
-  if (titleTask == "") {
-    alert("Complete field");
-  } else {
-    let newTask = document.getElementById("tasks");
-    newTask.innerHTML += templateTask;
-    taskList.push(titleTask);
-  }
-};
+      //Task item List
+      var taskLi = document.querySelector("#taskLi");
+      taskLi.setAttribute("id", id++);
+
+      //Button Done
+      const buttonTaskDone = document.createElement("button");
+      buttonTaskDone.classList.add('removeButton');
+      buttonTaskDone.innerHTML = '<i class="fas fa-check-circle"></i>';
+
+      //Add button in task item
+      taskLi.appendChild(buttonTaskDone);
+
+      //Pending Task amount
+      let pendingAmount = document.querySelector("#amountPending");
+      taskListPending.push(titleTask);
+      pendingAmount.innerHTML = taskListPending.length;
+    }
+  };
+})
+
